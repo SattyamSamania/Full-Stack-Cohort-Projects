2f393ea9a8a461949a99bb32f0e9dcdb503e0c25
Sattyam Samania <Sattyamsamania2810@gmail.com>
2024-07-09 12:45:19 +0530

    Create navbar for the Page

--- week8-Dukaan/Dukaan/src/App.jsx
+++ week8-Dukaan/Dukaan/src/App.jsx
@@ -1,41 +1,81 @@
-import React from 'react'
-import RevenueCard from './components/RevenueCard'
-import OrderTable from './components/OrderTable'
-import Heading from './components/Heading'
+import React from "react";
+import RevenueCard from "./components/RevenueCard";
+import OrderTable from "./components/OrderTable";
+import Heading from "./components/Heading";
+import Navbar from "./components/Navbar";
+import Dashboard from "./components/Dashboard";
 
 const App = () => {
   return (
     <>
-    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 m-10'>
-      <RevenueCard title={"Next payout"} amount={"2,312.23"} orderCount={23} className="bg-red-800" ispecial={true} payoutDate={"Next payout date:"} time={"Today 4:00 PM"} />
+      <div className="flex">
+        <aside className="fixed mt-0">
+          <Dashboard />
+        </aside>
+        <main className=" w-full ml-60">
+          <Navbar />
 
-      <RevenueCard title={"Amount pending"} amount={"92,312.20"} orderCount={13} />
-      <RevenueCard title={"Amount processed"} amount={"23,92,312.19"} />
+          <div className="mt-10 mx-10 flex justify-between text-xl">
+            <h3>Overview</h3>
+            <div className="flex items-center border-2 border-gray p-2 rounded">
+              <button>This month </button>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="m19.5 8.25-7.5 7.5-7.5-7.5"
+                />
+              </svg>
+            </div>
+          </div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-3 mx-10 ">
+            <RevenueCard
+              title={"Next payout"}
+              amount={"2,312.23"}
+              orderCount={23}
+              className="bg-red-800"
+              ispecial={true}
+              payoutDate={"Next payout date:"}
+              time={"Today 4:00 PM"}
+            />
 
-     
+            <RevenueCard
+              title={"Amount pending"}
+              amount={"92,312.20"}
+              orderCount={13}
+            />
+            <RevenueCard title={"Amount processed"} amount={"23,92,312.19"} />
+          </div>
 
-    </div>
+          <div className="m-10">
+            <h2 className="font-bold ">Transactions | This Month</h2>
+            <div className="">
+              <button className="text-[#808080] bg-[#E6E6E6] p-2.5 rounded-full ">
+                Payouts(22)
+              </button>
+              <button className="bg-[#146EB4]  text-white p-2.5 rounded-full ml-2">
+                Refunds(6)
+              </button>
+            </div>
 
-<div className='m-10'>
+            {/* OrderTable Starts here  */}
 
+            <div className="">
+              <Heading />
+              <OrderTable />
+            </div>
+          </div>
+        </main>
+      </div>
+    </>
+  );
+};
 
-<h2 className='font-bold '>Transactions | This Month</h2>
-<div className=''>
-  <button className='text-[#808080] bg-[#E6E6E6] p-2.5 rounded-full '>Payouts(22)</button>
-<button className='bg-[#146EB4]  text-white p-2.5 rounded-full ml-2'>Refunds(6)</button>
-</div>
-
-{/* OrderTable Starts here  */}
-
-<div className=''>
-
-<Heading/>
-<OrderTable/>
-
-</div>
-</div>
-</>
-  )
-}
-
-export default App
+export default App;
---
+++ week8-Dukaan/Dukaan/src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className='flex justify-between m-10'>
+
+        <div className='flex gap-2'>
+            <p className='text-xl font-medium'>Payouts</p>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z" />
+</svg>
+<p>How it works </p>
+        </div>
+        <div className='flex bg-gray-200 text-black items-center  p-2' >
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6" >
+  <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+</svg>
+            <input type="text" placeholder='Search features, tutorials etc' className='bg-gray-200 text-black ' />
+        </div>
+        <div className='flex items-center gap-4 m-2'>
+     <div className='bg-[#e6e6e6] w-10 h-10 p-2 rounded-full'>   <svg   xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+  <path fill-rule="evenodd" d="M4.804 21.644A6.707 6.707 0 0 0 6 21.75a6.721 6.721 0 0 0 3.583-1.029c.774.182 1.584.279 2.417.279 5.322 0 9.75-3.97 9.75-9 0-5.03-4.428-9-9.75-9s-9.75 3.97-9.75 9c0 2.409 1.025 4.587 2.674 6.192.232.226.277.428.254.543a3.73 3.73 0 0 1-.814 1.686.75.75 0 0 0 .44 1.223ZM8.25 10.875a1.125 1.125 0 1 0 0 2.25 1.125 1.125 0 0 0 0-2.25ZM10.875 12a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Zm4.875-1.125a1.125 1.125 0 1 0 0 2.25 1.125 1.125 0 0 0 0-2.25Z" clip-rule="evenodd" />
+</svg>
+</div>
+<div className='bg-[#e6e6e6] w-10 h-10 p-2 rounded-full'>
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+</svg>
+</div>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar
