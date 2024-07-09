d92233d33e69aae8971f06a1e04c85af5cf81069
Sattyam Samania <Sattyamsamania2810@gmail.com>
2024-07-09 18:14:54 +0530

    Add logo in Dashboard section

--- week8-Dukaan/Dukaan/src/App.jsx
+++ week8-Dukaan/Dukaan/src/App.jsx
@@ -6,13 +6,22 @@ import Navbar from "./components/Navbar";
 import Dashboard from "./components/Dashboard";
 
 const App = () => {
+
+
   return (
     <>
-      <div className="flex">
-        <aside className="fixed ">
-          <Dashboard />
+      <div className="flex ">
+      <div className="md:hidden m-4  cursor-pointer">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
+</svg>
+
+        </div>
+        <aside className="fixed hidden md:block ">
+       
+          <Dashboard  />
         </aside>
-        <main className=" w-full ml-60">
+        <main className=" w-full md:ml-60">
           <Navbar />
 
           <div className="mt-10 mx-10 flex justify-between text-xl">
