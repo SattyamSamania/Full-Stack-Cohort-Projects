95dc417c769bd9766896102194f26a83c2b66b35
Sattyam Samania <Sattyamsamania2810@gmail.com>
2024-07-09 09:51:57 +0530

    Add Revenue Card and orderTable

--- week8-Dukaan/Dukaan/src/App.jsx
+++ week8-Dukaan/Dukaan/src/App.jsx
@@ -1,11 +1,40 @@
 import React from 'react'
 import RevenueCard from './components/RevenueCard'
+import OrderTable from './components/OrderTable'
+import Heading from './components/Heading'
 
 const App = () => {
   return (
-    <div className='grid grid-cols-4 shadow-sm'>
+    <>
+    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 m-10'>
+      <RevenueCard title={"Next payout"} amount={"2,312.23"} orderCount={23} className="bg-red-800" ispecial={true} payoutDate={"Next payout date:"} time={"Today 4:00 PM"} />
+
       <RevenueCard title={"Amount pending"} amount={"92,312.20"} orderCount={13} />
+      <RevenueCard title={"Amount processed"} amount={"23,92,312.19"} />
+
+     
+
     </div>
+
+<div className='m-10'>
+
+
+<h2 className='font-bold '>Transactions | This Month</h2>
+<div className=''>
+  <button className='text-[#808080] bg-[#E6E6E6] p-2.5 rounded-full '>Payouts(22)</button>
+<button className='bg-[#146EB4]  text-white p-2.5 rounded-full ml-2'>Refunds(6)</button>
+</div>
+
+{/* OrderTable Starts here  */}
+
+<div className=''>
+
+<Heading/>
+<OrderTable/>
+
+</div>
+</div>
+</>
   )
 }
 
---
+++ week8-Dukaan/Dukaan/src/components/OrderTable.jsx
@@ -0,0 +1,309 @@
+import React from 'react'
+
+const OrderTable = () => {
+  return (
+    <>
+    <section>
+
+    <div className='grid grid-cols-5 gap-4 m-10'>
+
+ 
+<h4>ORDER ID</h4>
+<h4>STATUS</h4>
+
+<h4>TRANSACTION ID</h4>
+
+<h4>REFUND DATE</h4>
+
+<h4>ORDER AMOUNT</h4>
+
+
+   </div>
+
+   <div className='grid grid-cols-5 gap-4 m-10'>
+    <p className='text-[#146EB4]'> #281209</p>
+
+   <div className='flex items-center'>
+   <svg xmlns="http://www.w3.org/2000/svg" class="w-3 h-3" fill="green" viewBox="0 0 24 24">
+    <circle cx="12" cy="12" r="12" />
+  </svg>
+  <p>  Successfull</p>
+  </div>
+    <p>131634495747</p>
+    <p>Today, 08:45 PM</p>
+    <p>₹1,125.00</p>
+   
+
+		
+    </div>
+
+
+    <div className='grid grid-cols-5 gap-4 m-10'>
+    <p className='text-[#146EB4]'>  #281208</p>
+    <p>Processing</p>
+    <p>131634495747</p>
+    <p>Yesterday, 8:45 PM</p>
+    <p>	₹1,125.00</p>
+   
+   
+
+	
+		
+    </div>
+
+
+    <div className='grid grid-cols-5 gap-4 m-10'>
+    <p className='text-[#146EB4]'> #281207</p>
+    <div className='flex items-center'>
+    <svg xmlns="http://www.w3.org/2000/svg" class="w-3 h-3" fill="red" viewBox="0 0 24 24">
+    <circle cx="12" cy="12" r="12" />
+  </svg>
+    <p>  Failed</p>
+    
+    </div>
+    <p>131634495747</p>
+    <p>12 Jul 2023, 3:00 PM</p>
+    <p>₹1,125.00</p>
+   
+    
+
+		
+		
+    </div>
+
+
+    <div className='grid grid-cols-5 gap-4 m-10'>
+    <p className='text-[#146EB4]'> #281206</p>
+    <p>Successful</p>
+    <p>131634495747</p>
+    <p>12 Jul 2023, 3:00 PM</p>
+    <p>₹1,125.00</p>
+   
+    
+
+		
+    </div>
+
+    <div className='grid grid-cols-5 gap-4 m-10'>
+    <p className='text-[#146EB4]'> #281205</p>
+    <p>Processing</p>
+    <p>131634495747</p>
+    <p>12 Jul 2023, 3:00 PM</p>
+    <p>₹1,125.00</p>
+   
+    
+
+		
+    </div>
+
+    <div className='grid grid-cols-5 gap-4 m-10'>
+    <p className='text-[#146EB4]'>  #281204</p>
+    <p>Failed</p>
+    <p>131634495747</p>
+    <p>12 Jul 2023, 1:00 PM</p>
+    <p>₹1,125.00</p>
+   
+   
+
+		
+    </div>
+    <div className='grid grid-cols-5 gap-4 m-10'>
+    <p className='text-[#146EB4]'> #281203</p>
+    <p>Successful</p>
+    <p>131634495747</p>
+    <p>11 Jul 2023, 3:00 PM</p>
+    <p>₹1,125.00</p>
+ 
+		
+    </div>
+    <div className='grid grid-cols-5 gap-4 m-10'>
+    <p className='text-[#146EB4]'> #281202</p>
+    <p>Successful</p>
+    <p>131634495747</p>
+    <p>Today, 08:45 PM</p>
+    <p>₹1,125.00</p>
+   
+
+		
+    </div>
+    <div className='grid grid-cols-5 gap-4 m-10'>
+    <p className='text-[#146EB4]'> #281201</p>
+    <p>Successful</p>
+    <p>131634495747</p>
+    <p>Today, 08:45 PM</p>
+    <p>₹1,125.00</p>
+   
+
+		
+    </div>
+    <div className='grid grid-cols-5 gap-4 m-10'>
+    <p className='text-[#146EB4]'> #281200</p>
+    <p>Successful</p>
+    <p>131634495747</p>
+    <p>Today, 08:45 PM</p>
+    <p>₹1,125.00</p>
+   
+
+		
+    </div>
+    <div className='grid grid-cols-5 gap-4 m-10'>
+    <p className='text-[#146EB4]'> #281199</p>
+    <p>Successful</p>
+    <p>131634495747</p>
+    <p>Today, 08:45 PM</p>
+    <p>₹1,125.00</p>
+   
+
+		
+    </div>
+    <div className='grid grid-cols-5 gap-4 m-10'>
+    <p className='text-[#146EB4]'> #281198</p>
+    <p>Successful</p>
+    <p>131634495747</p>
+    <p>Today, 08:45 PM</p>
+    <p>₹1,125.00</p>
+   
+
+		
+    </div>
+    <div className='grid grid-cols-5 gap-4 m-10'>
+    <p className='text-[#146EB4]'> #281197</p>
+    <p>Successful</p>
+    <p>131634495747</p>
+    <p>Today, 08:45 PM</p>
+    <p>₹1,125.00</p>
+   
+
+		
+    </div>
+    <div className='grid grid-cols-5 gap-4 m-10'>
+    <p className='text-[#146EB4]'> #281196</p>
+    <p>Successful</p>
+    <p>131634495747</p>
+    <p>Today, 08:45 PM</p>
+    <p>₹1,125.00</p>
+   
+
+		
+    </div>
+    <div className='grid grid-cols-5 gap-4 m-10'>
+    <p className='text-[#146EB4]'> #281195</p>
+    <p>Successful</p>
+    <p>131634495747</p>
+    <p>Today, 08:45 PM</p>
+    <p>₹1,125.00</p>
+   
+
+		
+    </div>
+    <div className='grid grid-cols-5 gap-4 m-10'>
+    <p className='text-[#146EB4]'> #281194</p>
+    <p>Successful</p>
+    <p>131634495747</p>
+    <p>Today, 08:45 PM</p>
+    <p>₹1,125.00</p>
+   
+
+		
+    </div>
+    <div className='grid grid-cols-5 gap-4 m-10'>
+    <p className='text-[#146EB4]'> #281193</p>
+    <p>Successful</p>
+    <p>131634495747</p>
+    <p>Today, 08:45 PM</p>
+    <p>₹1,125.00</p>
+   
+
+		
+    </div>
+
+
+    <div className='grid grid-cols-5 gap-4 m-10'>
+    <p className='text-[#146EB4]'> #281192</p>
+    <p>Successful</p>
+    <p>131634495747</p>
+    <p>Today, 08:45 PM</p>
+    <p>₹1,125.00</p>
+   
+
+		
+    </div>
+    <div className='grid grid-cols-5 gap-4 m-10'>
+    <p className='text-[#146EB4]'> #281191</p>
+    <p>Successful</p>
+    <p>131634495747</p>
+    <p>Today, 08:45 PM</p>
+    <p>₹1,125.00</p>
+   
+
+		
+    </div>
+    <div className='grid grid-cols-5 gap-4 m-10'>
+    <p className='text-[#146EB4]'> #281190</p>
+    <p>Successful</p>
+    <p>131634495747</p>
+    <p>Today, 08:45 PM</p>
+    <p>₹1,125.00</p>
+   
+
+		
+    </div>
+    <div className='grid grid-cols-5 gap-4 m-10'>
+    <p className='text-[#146EB4]'> #281189</p>
+    <p>Successful</p>
+    <p>131634495747</p>
+    <p>Today, 08:45 PM</p>
+    <p>₹1,125.00</p>
+   
+
+		
+    </div>
+    <div className='grid grid-cols-5 gap-4 m-10'>
+    <p className='text-[#146EB4]'> #281188</p>
+    <p>Successful</p>
+    <p>131634495747</p>
+    <p>Today, 08:45 PM</p>
+    <p>₹1,125.00</p>
+   
+
+		
+    </div>
+    <div className='grid grid-cols-5 gap-4 m-10'>
+    <p className='text-[#146EB4]'> #281187</p>
+    <p>Successful</p>
+    <p>131634495747</p>
+    <p>Today, 08:45 PM</p>
+    <p>₹1,125.00</p>
+   
+
+		
+    </div>
+    <div className='grid grid-cols-5 gap-4 m-10'>
+    <p className='text-[#146EB4]'> #281186</p>
+    <div className='flex items-center'>
+    <svg xmlns="http://www.w3.org/2000/svg" class="w-3 h-3" fill="red" viewBox="0 0 24 24">
+    <circle cx="12" cy="12" r="12" />
+  </svg>
+    <p>  Successful</p>
+    
+    </div>
+    
+    <p>131634495747</p>
+    <p>Today, 08:45 PM</p>
+    <p>₹1,125.00</p>
+   
+
+		
+
+
+		
+    </div>
+
+
+    </section>
+     
+    </>
+  )
+}
+
+export default OrderTable
