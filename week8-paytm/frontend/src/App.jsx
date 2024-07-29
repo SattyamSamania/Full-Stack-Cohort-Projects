1b702a282c3968c40eac621eb0313fc0798add15
Sattyam Samania <Sattyamsamania2810@gmail.com>
2024-07-29 17:25:05 +0530

    Added Components in Frontend

--- week8-paytm/frontend/src/App.jsx
+++ week8-paytm/frontend/src/App.jsx
@@ -1,28 +1,26 @@
-import React from "react"
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Signup from "./pages/Signup"
-import Signin from "./pages/Signin"
-import Dashboard from "./pages/Dashboard"
-import SendMoney from "./pages/SendMoney"
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
+import Dashboard from "./pages/Dashboard";
+import SendMoney from "./pages/SendMoney";
+import Error from "./Error";
 
 function App() {
-
   return (
-   <>
-   
-   <BrowserRouter>
-   <Routes>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/Dashboard" element={<Dashboard />} />
+          <Route path="/SendMoney" element={<SendMoney />} />
 
-   <Route path="/signup" element={<Signup/>} />
-   <Route path="/signin" element={<Signin/>} />
-   <Route path="/Dashboard" element={<Dashboard/>} />
-   <Route path="/SendMoney" element={<SendMoney/>} />
-
-   </Routes>
-   
-   </BrowserRouter>
-   </>
-  )
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ week8-paytm/frontend/src/components/Balance.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Balance = ({value}) => {
+  return (
+    <>
+     <div className='font-semibold p-5 text-2xl'>
+        Your Balance {value}
+        </div> 
+    </>
+  )
+}
+
+export default Balance
